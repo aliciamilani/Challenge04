1d9efe8ca78a8cf98de09e3ebcc7b4690950188e
Victor Luiz <victor_luizsantos@hotmail.com>
2022-08-16 20:01:37 -0400

    fix: buttons

--- Challenge04/Challenge04/Controller/ViewController.swift
+++ Challenge04/Challenge04/Controller/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var mainTitle: UILabel!
     @IBOutlet weak var humorImage: UIImageView!
     @IBOutlet weak var humorTitle: UILabel!
@@ -20,13 +19,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var tiredBtn: UIButton!
     @IBOutlet weak var angerBtn: UIButton!
     
-    var goButton: Bool = false
-
+    
 
     @IBAction func humorButtons(_ sender: UIButton) {
         
         // Change button color when selected
-        var color = sender.backgroundColor
+        var color = sender.tintColor
         color = (color == UIColor.brown) ? UIColor.black : UIColor.brown
         
         if color == .black {
@@ -47,30 +45,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        humorTitle.text = "Confident"
-        //humorImage.image = UIImage(named: "")
-        mainTitle.text = "How are you feeling today?"
-        confidentBtn.titleLabel?.text = "Confident"
-        confidentBtn.backgroundColor = .brown
-        confidentBtn.alpha = 1.0
-        happyBtn.titleLabel?.text = "Happy"
-        happyBtn.backgroundColor = .black
-        happyBtn.alpha = 0.7
-        confidentBtn.titleLabel?.text = "Confident"
-        confidentBtn.alpha = 0.7
-        confidentBtn.backgroundColor = .black
-        sadBtn.titleLabel?.text = "Sad"
-        sadBtn.alpha = 0.7
-        sadBtn.backgroundColor = .black
-        impatientBtn.titleLabel?.text = "Impatient"
-        impatientBtn.alpha = 0.7
-        impatientBtn.backgroundColor = .black
-        tiredBtn.titleLabel?.text = "Tired"
-        tiredBtn.alpha = 0.7
-        tiredBtn.backgroundColor = .black
-        angerBtn.titleLabel?.text = "Anger"
-        angerBtn.alpha = 0.7
-        angerBtn.backgroundColor = .black
+        confidentBtn.layer.cornerRadius = 25
     }
 }
 
