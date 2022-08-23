1acfdb68a9d93905df10451773efbff556f8313c
Anna Milani <aavm.snf19@uea.edu.br>
2022-08-23 08:47:56 -0400

    chore: add swipe example

--- Challenge04/Challenge04/Controller/DailyTasksController.swift
+++ Challenge04/Challenge04/Controller/DailyTasksController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DailyTasksController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+class DailyTasksController : UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var wecolmeMessage: UILabel!
     
@@ -42,6 +42,75 @@ class DailyTasksController : UIViewController, UITableViewDelegate, UITableViewD
         cell.configure(title: data[indexPath.row].title, description: data[indexPath.row].description)
         return cell
     }
+    
+    func tableView(_ tableView: UITableView,
+                   leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal,
+                                        title: "Favourite") { [weak self] (action, view, completionHandler) in
+                                            self?.handleMarkAsFavourite()
+                                            completionHandler(true)
+        }
+        action.backgroundColor = .systemBlue
+        
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+    
+    func tableView(_ tableView: UITableView,
+                       trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // Archive action
+        let archive = UIContextualAction(style: .normal,
+                                         title: "Archive") { [weak self] (action, view, completionHandler) in
+                                            self?.handleMoveToArchive()
+                                            completionHandler(true)
+        }
+        archive.backgroundColor = .systemGreen
+
+        // Trash action
+        let trash = UIContextualAction(style: .destructive,
+                                       title: "Trash") { [weak self] (action, view, completionHandler) in
+                                        self?.handleMoveToTrash()
+                                        completionHandler(true)
+        }
+        trash.backgroundColor = .systemRed
+
+        // Unread action
+        let unread = UIContextualAction(style: .normal,
+                                       title: "Mark as Unread") { [weak self] (action, view, completionHandler) in
+                                        self?.handleMarkAsUnread()
+                                        completionHandler(true)
+        }
+        unread.backgroundColor = .systemOrange
+
+        let configuration = UISwipeActionsConfiguration(actions: [trash, archive, unread])
+
+        return configuration
+    }
+    
+    
+    func tableView(_ tableView: UITableView,
+                   editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    
+}
+
+extension DailyTasksController: UITableViewDelegate {
+    private func handleMarkAsFavourite() {
+        print("Marked as favourite")
+    }
+
+    private func handleMarkAsUnread() {
+        print("Marked as unread")
+    }
+
+    private func handleMoveToTrash() {
+        print("Moved to trash")
+    }
+
+    private func handleMoveToArchive() {
+        print("Moved to archive")
+    }
+    
 }
 
 func getCurrentTime() -> String {
@@ -58,3 +127,5 @@ func getCurrentTime() -> String {
     let thedate = "\(stringDate)"
     return thedate
 }
+
+
