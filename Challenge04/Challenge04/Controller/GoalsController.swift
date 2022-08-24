4a7e9c0a28b845354731700ca7e38b2eba24251a
Victor Luiz <victor_luizsantos@hotmail.com>
2022-08-24 18:33:06 -0400

    feat: added buttons, autolayout done, denied/active confirm button

--- Challenge04/Challenge04/Controller/GoalsController.swift
+++ Challenge04/Challenge04/Controller/GoalsController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 class GoalsController: UIViewController {
     
+    
+    @IBOutlet weak var study1Btn: UIButton!
+    @IBOutlet weak var study2Btn: UIButton!
+    @IBOutlet weak var study3Btn: UIButton!
+    @IBOutlet weak var family1Btn: UIButton!
+    @IBOutlet weak var family2Btn: UIButton!
+    @IBOutlet weak var family3Btn: UIButton!
+    @IBOutlet weak var sport1Btn: UIButton!
+    @IBOutlet weak var sport2Btn: UIButton!
+    @IBOutlet weak var sport3Btn: UIButton!
+    @IBOutlet weak var arts1Btn: UIButton!
+    @IBOutlet weak var arts2Btn: UIButton!
+    @IBOutlet weak var arts3Btn: UIButton!
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        confirmButton.isEnabled = false
+        confirmButton.alpha = 0.5
+        
+    }
+    @IBAction func confirmAction(_ sender: UIButton) {
+        
+        
+    }
+    @IBAction func goalsChoice(_ sender: UIButton) {
+        confirmButton.alpha = 1.0
+        confirmButton.isEnabled = true
         
     }
 }
