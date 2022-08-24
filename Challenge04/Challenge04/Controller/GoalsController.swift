7feb655653362c905df287202c8631a09ac4eb3e
Victor Luiz <victor_luizsantos@hotmail.com>
2022-08-24 10:51:26 -0400

    chore: backup scroll

---
+++ Challenge04/Challenge04/Controller/GoalsController.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsController.swift
+//  Challenge04
+//
+//  Created by Victor Santos on 8/24/22.
+//
+
+import Foundation
+import UIKit
+
+class GoalsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
