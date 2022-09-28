ead94b1a0f3970fbb64b2684969e4cbc2d1817d5
Anna Milani <aavm.snf19@uea.edu.br>
2022-09-28 10:07:39 -0400

    chore: add LaunchScreen

--- Challenge04/Challenge04/AppDelegate.swift
+++ Challenge04/Challenge04/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        Thread.sleep(forTimeInterval: 0.3)
+        
         // Override point for customization after application launch.
         return true
     }
