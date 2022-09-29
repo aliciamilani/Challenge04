a5a6b797ac94449c893742a124177a7f31873ddc
Anna Milani <aavm.snf19@uea.edu.br>
2022-09-29 11:03:11 -0400

    chore: add onboarding screen

---
+++ Challenge04/Challenge04/Controller/OnboardingController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingController.swift
+//  GoMoo
+//
+//  Created by Anna Alicia Milani on 29/09/22.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class OnboardingController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
