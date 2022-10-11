a469464241595148b4886b737f91e10fa57e1eb8
Anna Milani <aavm.snf19@uea.edu.br>
2022-10-11 11:35:08 -0400

    fix: add file to deal with humor and change enum name

---
+++ Challenge04/Challenge04/Model/HumorTypes.swift
@@ -0,0 +1,17 @@
+//
+//  Humor.swift
+//  GoMoo
+//
+//  Created by Anna Alicia Milani on 11/10/22.
+//
+
+import Foundation
+
+public enum HumorTypes : String {
+    case happy = "Happy"
+    case confident = "Confident"
+    case indifferent = "Indifferent"
+    case irritated = "Irritated"
+    case tired = "Tired"
+    case sad = "Sad"
+}
